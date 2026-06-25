5921ff54adf17210b9cb92212d5996386dc780cc
Bemnyyy <benjamin.ristau@gmx.de>
2026-06-25 23:09:09 +0200

    Minor changes added to main.py for a better terminal overview

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from analyzer import process_daily_data
 
 def nightly_job():
     # Works on the previous day, runs at night
-    print("\n=== START: DATENVERARBEITUNG & EXPORT ===")
+    print("\n===== DATENVERARBEITUNG =====")
     rt_data = get_and_clear_daily_data()
     process_daily_data(rt_data)
-    print("\n=== ENDE: DATENVERARBEITUNG & EXPORT ===\n")
 
 # scheduling-setup for a real live run
 schedule.every(1).minutes.do(fetch_gtfs_rt)
@@ -21,11 +20,11 @@ if __name__ == "__main__":
     # loading given realtime data from .pb file to DB-table
     fetch_gtfs_rt()
     
-    print("2. Starte Analyse...")
+    print("2. Analyse wird gestartet...")
     # clearing DB-table, working on static-realtime-comparisson and building csv
     nightly_job()
 
-    print("=== Initialer Durchlauf komplett! CSV-Dateien sind fertig. ===")
+    print("===== Initialer Durchlauf komplett! CSV-Dateien sind fertig. =====")
     print("\nGehe in regulären Zeitplan über...")
     print("Warte auf geplante Ausführung... (Strg + C zum Beenden)")
 
